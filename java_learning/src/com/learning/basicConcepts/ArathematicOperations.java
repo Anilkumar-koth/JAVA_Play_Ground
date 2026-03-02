dab1bd089219e709e4248213f96480c46df02b8e
Anilkumar-koth <venkataanilkumar369@gmail.com>
2026-03-03 02:12:04 +0530

    Update ArathematicOperations.java

--- java_learning/src/com/learning/basicConcepts/ArathematicOperations.java
+++ java_learning/src/com/learning/basicConcepts/ArathematicOperations.java
@@ -13,10 +13,12 @@ public class ArathematicOperations{
 		int number1 = sc.nextInt();
 		int number2 = sc.nextInt();
 		int result = number1 + number2;
-		System.out.println(result);
+		System.out.println(number1 + "+" +number2 + "="+ result);
 		
 		int result2 = number1 - number2;
-		
+		System.out.println(result2);
+		int result3 = number1 * number2;
+		System.out.println(result3);
 
 	}
 }
